40ec380c004cb994845f74af0cf613e91a2a44f5
tbh <towhidur.rahman.bhuiyan@fokus.fraunhofer.de>
2021-08-16 12:59:21 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from ConnectionFactory import *
+from KafkaFactory import *
+
+
+
+if __name__ = '__main__':
+    config_file_path = 'config/pg_config.json'
+    conn = ConnectionFactory(config_file_path)
+    conn.grant_permission()
+    conn.kafka_connect_setup()
+    conn.create_tables()
+
+    kafka_conn = KafkaFactory()
+    kafka_conn.produce_msg()
+    kafka_conn.consume_msg()
+    
+
+
+
